f07ed0676b49a6ce64cd654f303257d286ecf6db
Sami Rautalahti <sami.rautalahti@student.lab.fi>
2023-03-09 13:55:29 +0200

    Update 14:32 first Android-app "Hello World"

--- app/src/main/java/com/example/my_application/MainActivity.java
+++ app/src/main/java/com/example/my_application/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.my_application;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button1;
+    private TextView textView2;
+
+    public static final String TAG ="MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.button1);
+
+        final Button button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                textView2.setText(R.string.app_name);
+
+                textView2.setVisibility(View.INVISIBLE);
+
+                Log.i(TAG, "Game button clicked");
+            }
+        });
+
+        textView2 = findViewById(R.id.textView2);
+
     }
 }

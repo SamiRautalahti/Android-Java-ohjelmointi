50d4deb4223b5df000aa64eb02919cbbf0c44ec1
Sami Rautalahti <sami.rautalahti@student.lab.fi>
2023-04-20 16:46:30 +0300

    updated 20.4.2023

--- app/src/main/java/YTJ/DataActivity.java
+++ app/src/main/java/YTJ/DataActivity.java
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.SearchView;
 
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
@@ -55,7 +61,42 @@ public class DataActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
 
         APIGet(url);
+
+        handleIntent(getIntent());
     }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            //use the query to search your data somehow
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        inflater.inflate(R.menu.options_menu, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }
+
+
 
     void APIGet(String url){
         RequestQueue requestQueue;
@@ -82,8 +123,11 @@ public class DataActivity extends AppCompatActivity {
 
                                 itemList.add(item);
 
-                                Log.i(TAG, "Name: " + companyName + ", Registration Date: " + registrationDate + ", Company Form: " + companyForm + ", Business ID: " + businessId);
+
+
+                                //Log.i(TAG, "Name: " + item.name + ", Registration Date: " + item.registrationDate + ", Company Form: " + item.companyForm + ", Business ID: " + item.businessId);
                             }
+
 
                             Log.e(TAG,"Number of results: " + responseItems.length());
                         } catch (JSONException e) {
--- app/src/main/java/YTJ/RecyclerAdapter.java
+++ app/src/main/java/YTJ/RecyclerAdapter.java
@@ -1,9 +1,14 @@
 package YTJ;
 
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,9 +18,23 @@ import com.example.my_application.R;
 
 import java.util.ArrayList;
 
-public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> {
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> implements Filterable {
 
     private ArrayList<Item> dataSet;
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                FilterResults filterResults = new FilterResults();
+                return filterResults;
+            }
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            }
+        };
+    }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
 
@@ -28,6 +47,9 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
 
         ImageView imageViewIcon;
 
+        LinearLayout visible;
+        LinearLayout hidden;
+
         public MyViewHolder(View itemView) {
             super(itemView);
             this.textViewName = (TextView) itemView.findViewById(R.id.textViewName);
@@ -35,8 +57,27 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
             this.textViewDate = (TextView) itemView.findViewById(R.id.textViewDate);
             this.textViewForm = (TextView) itemView.findViewById(R.id.textViewForm);
             //this.imageViewIcon = (ImageView) itemView.findViewById(R.id.imageView);
+
+            this.visible= (LinearLayout) itemView.findViewById(R.id.visible);
+            this.hidden= (LinearLayout) itemView.findViewById(R.id.hidden);
+
+            visible.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (hidden.getVisibility() == view.VISIBLE) {
+                        TransitionManager.beginDelayedTransition(hidden, new AutoTransition());
+                        hidden.setVisibility(View.GONE);
+                    } else {
+                        TransitionManager.beginDelayedTransition(hidden, new AutoTransition());
+                        hidden.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+            );
         }
     }
+
+
 
     public RecyclerAdapter(ArrayList<Item> data) {
         this.dataSet = data;
